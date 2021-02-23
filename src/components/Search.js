2a5c025fb30cb4ba2f68881c90b4e3976ea26adc
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-02-23 20:42:41 +0100

    install axios, update npm, import useEffect in Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+
+  console.log("I RUN WITH EVERY RENDER");
+
+  useEffect(() => {
+    console.log("I RUN AFTER EVERY RENDER AS WELL AS INITIALLY if the data has changed!");
+  }, [term]);
+
   return (
     <div>
       <div className="ui form">
