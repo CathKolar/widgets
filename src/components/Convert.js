e0a50fc4c03659347a9642d2de65383ebeb0b944
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-04-22 13:57:11 +0200

    display translated text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,18 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Convert({ language, text }) {
+  const [transated, setTranslated] = useState("");
+
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
   }, [language, text]);
-  return null;
+  return (
+    <div>
+      <h1 className="ui header">{transated}</h1>
+    </div>
+  );
 }
