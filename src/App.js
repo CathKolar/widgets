88e2e82cd90e0db73025b3736a0b2112ae7bd112
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-04-25 13:50:17 +0200

    create and utilize reusable Routing component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 // eslint-disable-next-line no-unused-vars
 import Footer from "./components/Footer";
-// eslint-disable-next-line no-unused-vars
 import Search from "./components/Search";
-// eslint-disable-next-line no-unused-vars
 import Dropdown from "./components/Dropdown";
-// eslint-disable-next-line no-unused-vars
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
-// eslint-disable-next-line no-unused-vars
 const items = [
   {
     title: "What is React?",
@@ -30,38 +27,27 @@ const options = [
   { label: "A Shade of Blue", value: "blue" },
 ];
 const App = () => {
-  // eslint-disable-next-line no-unused-vars
   const [selected, setSelected] = useState(options[0]);
-
-  const showAccordion = () => {
-    if (window.location.pathname === "/") {
-      return <Accordion items={items} />;
-    }
-  };
-  const showList = () => {
-    if (window.location.pathname === "/list") {
-      return <Search />;
-    }
-  };
-
-  const showDropdown = () => {
-    if (window.location.pathname === "/dropdown") {
-      return <Dropdown />;
-    }
-  };
-
-  const showTranslate = () => {
-    if (window.location.pathname === "/translate") {
-      return <Translate />;
-    }
-  };
 
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
