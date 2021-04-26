f01785c13ca0a150f01483904b0f3202f42a33bf
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-04-26 13:31:17 +0200

    route detect navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,6 +4,8 @@ export default function Link({ href, className, children }) {
   const onClick = (event) => {
     event.preventDefault();
     window.history.pushState({}, "", href);
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
   return (
     <a onClick={onClick} href={href} className={className}>
